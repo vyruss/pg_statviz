34b7152b723ace097cd0ddc45412224284113c18
Rajiv Harlalka <68731551+rajivharlalka@users.noreply.github.com>
2023-06-03 17:56:30 +0530

    add locks module

--- pg_statviz--0.1--0.2.sql
+++ pg_statviz--0.1--0.2.sql
@@ -49,3 +49,47 @@ CREATE OR REPLACE FUNCTION @extschema@.snapshot_uptime(snapshot_tstamp timestamp
   $$ LANGUAGE SQL;
 
   
+-- Locks
+CREATE TABLE IF NOT EXISTS @extschema@.lock(
+    snapshot_tstamp timestamptz REFERENCES @extschema@.snapshots(snapshot_tstamp) ON DELETE CASCADE PRIMARY KEY,
+    lock_total int,
+    lock_access_share int,
+    lock_row_share int,
+    lock_row_ex int,
+    lock_share_update_ex int,
+    lock_share int,
+    lock_share_row_ex int,
+    lock_ex int,
+    lock_access_ex int);
+
+CREATE OR REPLACE FUNCTION @extschema@.snapshot_lock(snapshot_tstamp timestampz)
+RETURNS VOID
+AS $$
+  WITH
+    pgl AS (
+      SELECT mode
+       FROM pg_locks)
+  INSERT INTO @extschema@.lock (
+    snapshot_tstamp,
+    lock_total,
+    lock_access_share ,
+    lock_row_share ,
+    lock_row_ex ,
+    lock_share_update_ex ,
+    lock_share ,
+    lock_share_row_ex ,
+    lock_ex ,
+    lock_access_ex) 
+  SELECT 
+    snapshot_tstamp,
+    count(*) AS lock_total,
+    count(*) FILTER (WHERE mode = 'AccessShareLock') as lock_access_share,
+    count(*) FILTER (WHERE mode = 'RowShareLock') as lock_row_share,
+    count(*) FILTER (WHERE mode = 'RowExclusiveLock') as lock_row_ex,
+    count(*) FILTER (WHERE mode = 'ShareUpdateExclusiveLock') as lock_share_update_ex,
+    count(*) FILTER (WHERE mode = 'ShareLock') as lock_share,
+    count(*) FILTER (WHERE mode = 'ShareRowExclusiveLock') as lock_share_row_ex,
+    count(*) FILTER (WHERE mode = 'ExclusiveLock') as lock_ex,
+    count(*) FILTER (WHERE mode = 'AccessExclusiveLock') as lock_access_ex
+  FROM pgl;
+$$ LANGUAGE SQL;
