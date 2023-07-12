299039c481ad349f47b9c1f9df8dcf73e1d6f2b2
Jimmy Angelakos <jimmy.angelakos@enterprisedb.com>
2023-07-12 23:03:09 +0100

    Improved hostname error handling as per Rajiv Harlalka's suggestion

--- src/pg_statviz/libs/info.py
+++ src/pg_statviz/libs/info.py
@@ -7,7 +7,7 @@ __copyright__ = "Copyright (c) 2023 Jimmy Angelakos"
 __license__ = "PostgreSQL License"
 
 import logging
-import psycopg2
+import psycopg2.errors
 
 
 logging.basicConfig()
@@ -26,16 +26,16 @@ def getinfo(conn):
         if not cur.fetchone():
             raise SystemExit("pg_statviz extension is not installed in this "
                              + "database")
-        cur.execute("""CREATE TEMP TABLE info(hostname text);
-                       COPY info FROM PROGRAM 'hostname';
+        cur.execute("""CREATE TEMP TABLE _info(hostname text);
+                       COPY _info FROM PROGRAM 'hostname';
                        SELECT hostname,
                               inet_server_addr(),
                               current_setting('block_size')
-                       FROM info""")
+                       FROM _info""")
         info['hostname'], info['inet_server_addr'], info['block_size'] \
             = cur.fetchone()
         cur.close()
-    except psycopg2.errors.InsufficientPrivilege as e:
+    except (ExternalRoutineException, InsufficientPrivilege) as e:
         conn.rollback()
         cur = conn.cursor()
         _logger.error(e)
